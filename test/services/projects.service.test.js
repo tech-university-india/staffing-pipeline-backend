8ba1983884b29a21ed9392d3c2a5523abc1e7b2e
Sneha <snehalall.sneha@gmail.com>
2023-02-09 19:25:01 +0530

    fix: add linting to the get project files

--- test/services/projects.service.test.js
+++ test/services/projects.service.test.js
@@ -1,10 +1,7 @@
 const { engagements } = require('../../src/models');
 const getProjectServices = require('../../src/services/projectServices');
 
-
 describe('Engagement Services', () => {
-  
-
   it('should return the engagement details of the provided id from the database', async () => {
     jest.spyOn(engagements, 'findByPk').mockResolvedValue([
       {
@@ -13,11 +10,8 @@ describe('Engagement Services', () => {
         case_study_ids: ['23', '34', '56']
 
       }
-
-
     ]
     );
-
     const project = await getProjectServices.getProject();
     expect(project).toEqual(
       [
@@ -31,7 +25,4 @@ describe('Engagement Services', () => {
       ]
     );
   });
-
-  
-
 });    
