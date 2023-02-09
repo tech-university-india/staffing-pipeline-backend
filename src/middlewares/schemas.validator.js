f3551fe53ab328e264b3728703c63eade0bdb7db
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-02-09 23:48:39 +0530

    feat(users): use Joi validation for user_id param as middleware

--- src/middlewares/schemas.validator.js
+++ src/middlewares/schemas.validator.js
@@ -1,22 +1,7 @@
-
-// require joi
 const Joi = require('joi');
-const uuidType = Joi.string().uuid({
-  version: [
-    'uuidv4',
-    'uuidv1'
-  ]
-});
 
-const userSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  fmno: Joi.number().integer().required(),
-  skillId: uuidType.required(),
-  caseStudyId: uuidType.required(),
-  studyId: uuidType.required(),
-  roleId: uuidType.required(),
-  guildId: uuidType.required()
-});
+const userIdSchema = Joi.object({
+  user_id: Joi.string().required().regex(/^[0-9]*$/)
+}).required();
 
-module.exports = { userSchema };
+module.exports = { userIdSchema };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const { getUsers, getUserById } = require('../controllers/user.controller');
 
+const schemas = require('../middlewares/schemas.validator');
+const validator = require('../middlewares/request.validator');
 const router = express.Router();
 
 router.get('/users', getUsers);
-router.get('/users/:user_id', getUserById);
+router.get('/users/:user_id', validator(schemas.userIdSchema, 'params'), getUserById);
 
 module.exports = router;
