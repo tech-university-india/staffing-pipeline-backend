6ccd43708d0c601f0840fe2513ba0c1d80c2c2d0
rishusingh022 <rishusingh022@gmail.com>
2023-02-10 10:55:57 +0530

    chore : add test for update user controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -12,11 +12,12 @@ const updateUser = async (req, res) => {
     const { id } = req.params;
     const { body } = req;
     const updatedUser = await userServices.updateUser(id, body);
+    if(!updatedUser) res.status(404).json({ message: 'User not found' });
     res.status(200).json(updatedUser);
   }
   catch (error) {
     res.status(500).json({
-      message: error.message,
+      message: 'Something went wrong',
     });
   }
 };
