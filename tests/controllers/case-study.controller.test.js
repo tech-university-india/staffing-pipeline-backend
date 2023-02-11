a7f7e4323c3c49d9d02c903a5bddd7abaa58aff4
rishusingh022 <rishusingh022@gmail.com>
2023-02-12 02:18:50 +0530

    chore : add tests for delete caseStudy & services

--- tests/controllers/case-study.controller.test.js
+++ tests/controllers/case-study.controller.test.js
@@ -76,4 +76,63 @@ describe('CaseStudyController', () => {
       expect(mockRes.json).toHaveBeenCalledWith({ message: 'Something went wrong' });
     });
   });
+  describe('deleteCaseStudyController', () => {
+    it('should delete caseStudy', async () => {
+      const mockReq = {
+        params: {
+          id: '1',
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const resolvedValue = {
+        case_study_id: '1',
+        name: 'test',
+        description: 'test',
+        collaborators_ids: ['1', '2'],
+        image: 'test',
+        box_link: 'test',
+        engagement_id: '1',
+        createdAt: '2022-01-17T04:33:12.000Z',
+        updatedAt: '2022-01-17T04:33:12.000Z',
+      };
+      jest.spyOn(updateCaseStudyServices, 'deleteCaseStudy').mockResolvedValue(resolvedValue);
+      await updateCaseStudy.deleteCaseStudyController(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith(resolvedValue);
+    });
+    it('Should return 404 if caseStudy not found', async () => {
+      const mockReq = {
+        params: {
+          id: '1',
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const resolvedValue = null;
+      jest.spyOn(updateCaseStudyServices, 'deleteCaseStudy').mockResolvedValue(resolvedValue);
+      await updateCaseStudy.deleteCaseStudyController(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(404);
+      expect(mockRes.json).toHaveBeenCalledWith({ message: 'CaseStudy not found' });
+    });
+    it('Should return 500 if something went wrong', async () => {
+      const mockReq = {
+        params: {
+          id: '1',
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      jest.spyOn(updateCaseStudyServices, 'deleteCaseStudy').mockRejectedValue(new Error('Something went wrong'));
+      await updateCaseStudy.deleteCaseStudyController(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(500);
+      expect(mockRes.json).toHaveBeenCalledWith({ message: 'Something went wrong' });
+    });
+  });
 });
