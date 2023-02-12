b55a482998b673ea0842f38517e218b8b638ede5
rishusingh022 <rishusingh022@gmail.com>
2023-02-12 12:00:20 +0530

    chore : add genric file for case study and user and resolved merge conflicts with feat/caseStudies/update

--- src/routes/case-study.route.js
+++ src/routes/case-study.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authMiddlewares = require('../middlewares/request.validator');
 const caseStudyValidator = require('../middlewares/case-study.validator');
 const { updateCaseStudyController, deleteCaseStudyController } = require('../controllers/case-study.controller');
+
 const router = express.Router();
 
 router.put(
