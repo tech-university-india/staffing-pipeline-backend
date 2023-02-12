17e1b9b531718a07aef8cb354796a44dcaeed5b6
khushilms <khushilms.cs19@bmsce.ac.in>
2023-02-12 11:16:01 +0530

    chore: pull main and resolve conflicts

--- src/utils/HttpError.js
+++ src/utils/HttpError.js
@@ -1,14 +1,7 @@
 class HttpError extends Error {
   constructor(message, code) {
-<<<<<<< HEAD
     super(message, code);
     this.statusCode = code;
   }
 }
 module.exports = HttpError;
-=======
-    super(message); //
-    this.code = code; //
-  }
-}
->>>>>>> 19bf866 (chore: formatting with prettier)
