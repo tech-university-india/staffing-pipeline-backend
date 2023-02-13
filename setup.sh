2dcc64dc765d07325b3ffe4c705ca7c9a7e416e8
khushilms <khushilms.cs19@bmsce.ac.in>
2023-02-13 15:13:47 +0530

    chore: create more generic error messages for setup shell script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
 npm install
 if [ $? -eq 1 ] ; then 
-  echo "**********************\nNpm not found\n**********************"
+  echo "**********************\nFailed to install dependencies\n**********************"
   exit 1
 fi
 npx sequelize-cli db:migrate
 if [ $? -eq 1 ] ; then 
-  echo "**********************\nDB Server not started\n**********************"
+  echo "**********************\nFailed to run migrations\n**********************"
   exit 1
 fi
 npx sequelize-cli db:seed:all
 if [ $? -eq 1 ] ; then 
-  echo "**********************\nSomething wrong with database\n**********************"
+  echo "**********************\nFailed to seed data to database\n**********************"
   exit 1
 fi
 npm start
+if [ $? -eq 1 ] ; then 
+  echo "**********************\nFailed to start server\n**********************"
+  exit 1
+fi
