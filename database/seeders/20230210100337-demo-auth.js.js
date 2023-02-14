0cae4d26801d6cc71f0e18fcff5c60347074bfa1
Promit-revar <promit.revar2211@gmail.com>
2023-02-14 11:23:34 +0530

    Feature/add seeders (#32)
    * chore: add seeders for the Database
    
    * fix: seeder data by replacing null values and providing generic data
    
    * chore: delete unwanted dependencies

--- database/seeders/20230210100337-demo-auth.js.js
+++ database/seeders/20230210100337-demo-auth.js.js
@@ -5,13 +5,15 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return await queryInterface.bulkInsert('auths', [
       {
-        email: 'promit.revar2211@gmail.com',
+
+        email: 'johndoe@mckinsey.com',
         password: '$2b$08$ap2OSCeWEBVlDY.xui2bPuoblmWTDQwetDvMHZF1B7k.NI3Ae5Tyq',
         updated_at: new Date(),
         created_at: new Date(),
       },
       {
-        email: 'param@gmail.com',
+
+        email: 'janedoe@mckinsey.com',
         password: '$2b$08$zmfjCVc5HbYlemPNDQwMPuiTZEGHY79hYQuCz/TtjUTWXcJ1TJs8m',
         updated_at: new Date(),
         created_at: new Date(),
