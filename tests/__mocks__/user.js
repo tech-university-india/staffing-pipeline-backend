4c6fb9ada6807c65136d29a07ad95b3895988fb4
rishusingh022 <rishusingh022@gmail.com>
2023-02-15 01:15:42 +0530

    chore : add mock data for user tests

---
+++ tests/__mocks__/user.js
@@ -0,0 +1,149 @@
+const createUser = {
+  mockRes: {
+    json: jest.fn(),
+    status: jest.fn().mockReturnThis(),
+  },
+  mockReq: {
+    body: {
+      email: 'promit.revar2211@gmail.com',
+      name: 'Promit Revar',
+    },
+  },
+  mockReqName: {
+    body: {
+      name: 'Promit Revar',
+    },
+  },
+  resolvedValue: {
+    data: {
+      user_id: '9a492c13-85e8-4b26-9339-a5d037664d1a',
+      email: 'promit.revar2211@gmail.com',
+      name: 'Promit Revar',
+      updatedAt: '2023-02-09T15:02:53.658Z',
+      createdAt: '2023-02-09T15:02:53.658Z',
+      fmno: null,
+      current_engagement_ids: null,
+      case_study_ids: null,
+      skills: null,
+      role: null,
+      guild: null,
+      past_engagement_ids: null,
+      image: null,
+    },
+    success: true,
+  },
+  newUser: {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    password: '12345',
+  },
+  errorMessage: 'notNull Violation: users.email cannot be null',
+  errorCode: 400,
+};
+const getUser = {
+  mockRes: {
+    status: jest.fn(),
+    json: jest.fn(),
+  },
+  mockReq: {
+    params: {
+      user_id: '1',
+    },
+  },
+  resolvedValue: {
+    user_id: '1',
+    name: 'john doe',
+    email: 'johndoe@mckinsey.com',
+    fmno: '123456',
+    current_engagement_ids: ['1', '2'],
+    case_study_ids: ['1', '2'],
+    skills: ['node, react'],
+    role: 'intern',
+    guild: 'swe',
+    past_engagement_ids: ['1', '2'],
+    createdAt: '2022-01-17T04:33:12.000Z',
+    updatedAt: '2022-01-17T04:33:12.000Z',
+  },
+};
+const deleteUser = {
+  mockReq: {
+    params: {
+      user_id: '1',
+    },
+  },
+  mockRes: {
+    status: jest.fn(),
+    json: jest.fn(),
+  },
+  resolvedValue: {
+    user_id: '1',
+    name: 'john doe',
+    email: 'johndoe@mckinsey.com',
+    fmno: '123456',
+    current_engagement_ids: ['1', '2'],
+    case_study_ids: ['1', '2'],
+    skills: ['node, react'],
+    role: 'intern',
+    guild: 'swe',
+    past_engagement_ids: ['1', '2'],
+    createdAt: '2022-01-17T04:33:12.000Z',
+    updatedAt: '2022-01-17T04:33:12.000Z',
+  },
+  deletedUser: {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    password: '12345',
+  },
+  mockUser: {
+    userIds: [11, 23],
+    dataValues: {
+      currentEngagementIds: [12, 13],
+      pastEngagementIds: [12, 13],
+    },
+    map: jest.fn(),
+  },
+  mockUsers: {
+    userId: 22,
+    currentEngagementIds: [122, 154],
+    pastEngagementIds: [12, 13],
+  },
+};
+const updateUser = {
+  mockReq: {
+    params: {
+      id: '1',
+    },
+    body: {
+      name: 'John Doe',
+      email: 'JohnDoe@mckinsey.com',
+      current_engagement_ids: ['1', '2', '3', '4'],
+    },
+  },
+  mockRes: {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  },
+  resolvedValue: {
+    user_id: '1',
+    name: 'John Doe',
+    email: 'JohnDoe@mckinsey.com',
+    fmno: '123456',
+    current_engagement_ids: ['1', '2', '3', '4'],
+    case_study_ids: ['1', '2'],
+    skills: ['node, react'],
+    role: 'intern',
+    guild: 'swe',
+    past_engagement_ids: ['1', '2'],
+    createdAt: '2022-01-17T04:33:12.000Z',
+    updatedAt: '2022-01-17T04:33:12.000Z',
+  },
+};
+
+module.exports = {
+  createUser,
+  getUser,
+  deleteUser,
+  updateUser,
+};
