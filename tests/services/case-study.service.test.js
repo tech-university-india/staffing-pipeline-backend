8cdc37e997558510ce190c92d7eec373c7cb111e
Sneha <snehalall.sneha@gmail.com>
2023-02-16 10:14:34 +0530

    fix : edit case-study-service test cases

--- tests/services/case-study.service.test.js
+++ tests/services/case-study.service.test.js
@@ -24,7 +24,7 @@ describe('Case Study Services', () => {
   });
 });
 
-describe('function setProjectAsNull', () => {
+describe('function removeProjectFromCaseStudy', () => {
   it('Should set engagement id as null in case study entity', async () => {
     const resolvedValue = { ...mockData.update.resolvedValue, update: jest.fn() };
     jest.spyOn(case_studies, 'update').mockResolvedValue(resolvedValue);
